f303148a85841d23bd15675e7bda559b65a4151f
Gerard <gerard.g.johnson@gmail.com>
2023-07-13 22:40:49 -0500

    mission test

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -7,11 +7,11 @@ Write a function called addTwo that takes in an array and adds two to every valu
 ------------------------------------------------------------------------------------------------ */
 
 const addTwo = (arr) => {
-  let arr2 = [];
-  arr2.forEach((n) => {
-    arr2.push(n + 2);
-  });
-  return arr2;
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i] + 2);
+  }
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -23,7 +23,7 @@ For example, typeNum([1, 'bob' ,3]) returns [1,3].
 ------------------------------------------------------------------------------------------------ */
 
 const typeNum = (arr) => {
-  // Solution code here...
+  return arr.filter((value) => typeof value === "number");
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -35,7 +35,7 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 ------------------------------------------------------------------------------------------------ */
 
 const containsAnd = (arr) => {
-  // Solution code here...
+  return arr.filter((str) => str.includes("and"));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -47,7 +47,7 @@ For example, oddValues([1,2,3]) returns [1,3].
 ------------------------------------------------------------------------------------------------ */
 
 const oddValues = (arr) => {
-  // Solution code here...
+  return arr.filter((num) => num % 2 !== 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -59,7 +59,7 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
-  // Solution code here...
+  return arr.filter((value) => !forbiddenValues.includes(value));
 };
 
 /* ------------------------------------------------------------------------------------------------
